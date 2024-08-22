280c4f1bdfdce1d8e4dd1c1d298b3c23d0dec376
Ganesh Chavan <124089466+GaneshChavan0506@users.noreply.github.com>
2024-08-22 12:12:20 +0530

    Update facts_trial_job.py

--- facts_trial_job.py
+++ facts_trial_job.py
@@ -1,13 +1,13 @@
 import sys
 from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
-from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
+from pyspark.context import SparkContext
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import year, month, dayofmonth, date_format, weekofyear, col
-from pyspark.sql import functions as F
-from pyspark.sql import Window
+from pyspark.sql.functions import when, col
+from pyspark.sql.types import IntegerType
+from pyspark.sql.types import StringType
 
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
 sc = SparkContext()
@@ -16,56 +16,52 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
-# Script generated for node Amazon S3
-AmazonS3_node1723532264557 = glueContext.create_dynamic_frame.from_options(format_options={"quoteChar": "\"", "withHeader": True, "separator": ","}, connection_type="s3", format="csv", connection_options={"paths": ["s3://flipcartdata/CSV_Data/"], "recurse": True}, transformation_ctx="AmazonS3_node1723532264557")
+#reading dataframe
+df = spark.read.csv("s3://flipcartdata/dim_data/", header=True, inferSchema=True)
 
-# Script generated for node Change Schema
-ChangeSchema_node1723532407097 = ApplyMapping.apply(frame=AmazonS3_node1723532264557, mappings=[("date_", "string", "date_", "date"), ("city_name", "string", "city_name", "string"), ("order_id", "string", "order_id", "int"), ("cart_id", "string", "cart_id", "int"), ("dim_customer_key", "string", "dim_customer_key", "int"), ("procured_quantity", "string", "procured_quantity", "int"), ("unit_selling_price", "string", "unit_selling_price", "double"), ("total_discount_amount", "string", "total_discount_amount", "double"), ("product_id", "string", "product_id", "int"), ("total_weighted_landing_price", "string", "total_weighted_landing_price", "double")], transformation_ctx="ChangeSchema_node1723532407097")
-
-# Convert the dynamic frame to a Spark DataFrame
-df = ChangeSchema_node1723532407097.toDF()
-
-#removing rows from procured quantity which contains 0
-df = df.filter(df.product_id != 0)
-
-# Calculate the total quantity sold per product
-df = df.withColumn(
-    "Total_Quantity_Sold",F.sum("procured_quantity").over(Window.partitionBy("product_id")).cast("bigint"))
-
-# Calculate the total sales per product
-df = df.withColumn(
-    "Total_Sales",F.sum(F.col("procured_quantity") * F.col("unit_selling_price")).over(Window.partitionBy("product_id")).cast("bigint"))
+df=df.drop("_c0")
+df = df.withColumn("product_id", df["product_id"].cast(IntegerType()))
+df = df.withColumn("l0_category_id", df["l0_category_id"].cast(IntegerType()))
+df = df.withColumn("l1_category_id", df["l1_category_id"].cast(IntegerType()))
+df = df.withColumn("l2_category_id", df["l2_category_id"].cast(IntegerType()))
 
 df = df.withColumn(
-    "Total_Revenue",
-     ((F.col("procured_quantity") * F.col("unit_selling_price")) - F.col("total_discount_amount")).cast("bigint"))
+    "brand_name",
+    when((col("product_type") == "Combo") & col("brand_name").isNull(), "Flipkart Combo")
+    .otherwise(col("brand_name"))
+).withColumn(
+    "manufacturer_name",
+    when((col("product_type") == "Combo") & col("manufacturer_name").isNull(), "Flipkart")
+    .otherwise(col("manufacturer_name"))
+)
 
-# Calculate the discount percentage for each row
-df = df.withColumn("Discount_Percentage", 
-                   F.round((F.col("total_discount_amount") / 
-                            (F.col("unit_selling_price") * F.col("procured_quantity"))) * 100))
 
-# Calculate the profit margin for each row
-df = df.withColumn("Profit_Margin", 
-                   F.round(((((F.col("procured_quantity") * F.col("unit_selling_price")) - 
-                              F.col("total_discount_amount")) - 
-                             F.col("total_weighted_landing_price")) / 
-                            (F.col("procured_quantity") * F.col("unit_selling_price"))) * 100))
-                            
-#adding new columns related to date functions
-df = df.withColumn("year_", year(col("date_"))) \
-       .withColumn("month_", month(col("date_"))) \
-       .withColumn("monthname_", date_format(col("date_"), "MMMM")) \
-       .withColumn("day_", dayofmonth(col("date_"))) \
-       .withColumn("weekday_", date_format(col("date_"), "EEEE")) \
-       .withColumn("weeknumber_", weekofyear(col("date_")))
+df = df.withColumn(
+    "brand_name",
+    when((col("brand_name").isNull()) & (col("manufacturer_name").isNull()), "Flipkart")
+    .otherwise(col("brand_name"))
+).withColumn(
+    "manufacturer_name",
+    when((col("brand_name").isNull()) & (col("manufacturer_name").isNull()), "Local")
+    .otherwise(col("manufacturer_name"))
+)
 
-df = df.dropna(subset=['Discount_Percentage', 'Profit_Margin'])
+df = df.withColumn(
+    "manufacturer_name",
+    when((col("manufacturer_name").isNull()) & (col("brand_name").isNotNull()), "Local")
+    .otherwise(col("manufacturer_name"))
+)
+
+df = df.withColumn(
+    "brand_name",
+    when((col("manufacturer_name").isNotNull()) & (col("brand_name").isNull()), "Local")
+    .otherwise(col("brand_name"))
+)
 
 # Coalesce to a single partition to ensure a single output file
 df = df.coalesce(1)
 
 # Write the merged DataFrame back to S3
-df.write.mode("overwrite").parquet("s3://cftcleandata/fact_cft/")
+df.write.mode("overwrite").parquet("s3://pirodata/dim_table/")
 
 job.commit()
