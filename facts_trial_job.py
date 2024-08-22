afb146781e269ee7c0db40a0d5d4f9c83317c1d5
Ganesh Chavan <124089466+GaneshChavan0506@users.noreply.github.com>
2024-08-22 13:32:14 +0530

    Update facts_trial_job.py

--- facts_trial_job.py
+++ facts_trial_job.py
@@ -5,9 +5,12 @@ from awsglue.context import GlueContext
 from awsglue.job import Job
 from pyspark.context import SparkContext
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import when, col
+from pyspark.sql.functions import year, month, dayofmonth, date_format, weekofyear, col
+from pyspark.sql import functions as F
+from pyspark.sql import Window
 from pyspark.sql.types import IntegerType
 from pyspark.sql.types import StringType
+from pyspark.sql.types import DoubleType
 
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
 sc = SparkContext()
@@ -16,52 +19,61 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
-#reading dataframe
-df = spark.read.csv("s3://flipcartdata/dim_data/", header=True, inferSchema=True)
+# Read all CSV files into a single DataFrame
+df = spark.read.csv("s3://flipcartdata/CSV_Data/", header=True, inferSchema=True)
 
 df=df.drop("_c0")
-df = df.withColumn("product_id", df["product_id"].cast(IntegerType()))
-df = df.withColumn("l0_category_id", df["l0_category_id"].cast(IntegerType()))
-df = df.withColumn("l1_category_id", df["l1_category_id"].cast(IntegerType()))
-df = df.withColumn("l2_category_id", df["l2_category_id"].cast(IntegerType()))
+df=df.drop("Unnamed: 0")
+
+df = df.withColumn("date_", col("date_").cast("date"))
+
+#removing rows from procured quantity which contains 0
+df = df.filter(df.procured_quantity != 0)
 
 df = df.withColumn(
-    "brand_name",
-    when((col("product_type") == "Combo") & col("brand_name").isNull(), "Flipkart Combo")
-    .otherwise(col("brand_name"))
+    "total_weighted_landing_price",
+    F.when(F.col("total_weighted_landing_price").isNull(), 0)
+    .otherwise(F.col("total_weighted_landing_price"))
+)
+
+# Calculate the total sales per product
+df = df.withColumn(
+    "Total_Sales",(F.col("procured_quantity") * F.col("unit_selling_price")).cast("double"))
+
+df = df.withColumn(
+    "Total_Revenue",
+     ((F.col("procured_quantity") * F.col("unit_selling_price")) - F.col("total_discount_amount")).cast("double"))
+
+# Calculate the discount percentage for each row
+df = df.withColumn("Discount_Percentage", 
+                   F.round((F.col("total_discount_amount") / 
+                            (F.col("unit_selling_price") * F.col("procured_quantity"))) * 100))
+
+# Calculate the profit margin for each row
+df = df.withColumn("Profit_Margin", 
+                   F.round(((((F.col("procured_quantity") * F.col("unit_selling_price")) - 
+                              F.col("total_discount_amount")) - 
+                             F.col("total_weighted_landing_price")) / 
+                            (F.col("procured_quantity") * F.col("unit_selling_price"))) * 100))
+                            
+#adding new columns related to date functions
+df = df.withColumn("monthname_", date_format(col("date_"), "MMMM")) \
+       .withColumn("weekday_", date_format(col("date_"), "EEEE"))
+
+df = df.withColumn(
+    "Discount_Percentage",
+    F.when(F.col("Discount_Percentage").isNull(), 0)
+    .otherwise(F.col("Discount_Percentage"))
 ).withColumn(
-    "manufacturer_name",
-    when((col("product_type") == "Combo") & col("manufacturer_name").isNull(), "Flipkart")
-    .otherwise(col("manufacturer_name"))
-)
-
-
-df = df.withColumn(
-    "brand_name",
-    when((col("brand_name").isNull()) & (col("manufacturer_name").isNull()), "Flipkart")
-    .otherwise(col("brand_name"))
-).withColumn(
-    "manufacturer_name",
-    when((col("brand_name").isNull()) & (col("manufacturer_name").isNull()), "Local")
-    .otherwise(col("manufacturer_name"))
-)
-
-df = df.withColumn(
-    "manufacturer_name",
-    when((col("manufacturer_name").isNull()) & (col("brand_name").isNotNull()), "Local")
-    .otherwise(col("manufacturer_name"))
-)
-
-df = df.withColumn(
-    "brand_name",
-    when((col("manufacturer_name").isNotNull()) & (col("brand_name").isNull()), "Local")
-    .otherwise(col("brand_name"))
+    "Profit_Margin",
+    F.when(F.col("Profit_Margin").isNull(), 0)
+    .otherwise(F.col("Profit_Margin"))
 )
 
 # Coalesce to a single partition to ensure a single output file
 df = df.coalesce(1)
 
 # Write the merged DataFrame back to S3
-df.write.mode("overwrite").parquet("s3://pirodata/dim_table/")
+df.write.mode("overwrite").parquet("s3://pirodata/fact_table/")
 
 job.commit()
